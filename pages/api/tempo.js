6a8015e8cc293e95744ed492c734258f6728e2cd
Kelvin Brito <kelvin_britto@hotmail.com>
2021-01-21 10:38:21 -0300

    tempo aula

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,13 @@
-function tempo(request, response) {
+async function tempo(request, response) {
     const dynamicDate = new Date();
 
+    const aulaAgora = await fetch("http://192.168.15.136:8081/aovivo");
+    const aulaAgoraJson = await aulaAgora.json();
+    const aula = aulaAgoraJson.aula;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        aula: aula
     })
 }
 
