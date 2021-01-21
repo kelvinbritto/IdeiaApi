3fdce6070c5dd76105414c54917e7723554c7f97
Kelvin Brito <kelvin_britto@hotmail.com>
2021-01-21 10:45:46 -0300

    tempo rua v2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,7 +2,7 @@ async function tempo(request, response) {
     const dynamicDate = new Date();
 
     const rua = await fetch("https://brasilapi.com.br/api/cep/v1/02543000");
-    const ruaJson = await aulaAgora.json();
+    const ruaJson = await rua.json();
     const ruaString = aulaAgoraJson.aula;
 
     response.json({
